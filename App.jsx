478c8df54b418f2e393350f0711c810a2f935b91
Sanjukulhare <sanjana26it053@satiengg.in>
2025-05-11 09:42:58 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Login from './Components/Login'
+
+const App = () => {
+  return (
+    <div className='grid w-[100%] h-screen place-items-center bg-gray-800'>
+      <Login />
+    </div>
+  )
+}
+
+export default App
